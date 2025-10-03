8a4e11aef52e73b42d88b1a3ff6e09d5b24beb1e
Avdyl Dinaj <avdyldinaj@gmail.com>
2025-10-03 04:54:45 +0200

    loading the dataset and making the basic preprocessing steps

---
+++ textclassification.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import neattext.functions as nfx
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+
+#dataset load and inspect
+df = pd.read_csv("C:/Users/Lenovo/Documents/Desktop/classification/netflix_users.csv")
+print(df.shape)
+print(df.info)
+print(df.head())
+
+#Preprocessing
+df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+
+output_path = "C:/Users/Lenovo/Documents/Desktop/classification/netflix_users_clean.csv"
+df.to_csv(output_path, index=False)
+
+
